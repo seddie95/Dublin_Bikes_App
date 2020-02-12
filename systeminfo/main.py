7f394106f08722a35924485076ad7e64f247e2a5
seddie95 <59086410+seddie95@users.noreply.github.com>
2020-02-12 19:47:21 +0000

    cleaned up the scrape, added new columns,removed unecessry variables and oop

--- systeminfo/main.py
+++ systeminfo/main.py
@@ -1,79 +1,72 @@
 import requests
 import json
 
-# global variables
 # create a new csv file to store the data
 file = open("bike_data.csv", "w")
 
 
-class BikeData():
-    def __init__(self):
-        self.getBikeData()
+def currentWeather(lat, lng):
+    """Function to call weather get weather data fom api """
+    url = 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lng + '&appid=9da3d1abfb8e1a3677d26c96350597c3&units=metric'
+    response = requests.get(url)
 
-    def currentWeather(self, lat, lng):
-        """Function to call weather get weather data fom api """
-        url = 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lng + '&appid=9da3d1abfb8e1a3677d26c96350597c3&units=metric'
-        response = requests.get(url)
+    data = response.json()
 
-        data = response.json()
+    # create variables to store the json information
+    temp = str(data['main']['temp'])
+    realfeel = str(int(data['main']['feels_like']))
+    minimum = str(data['main']['temp_min'])
+    maximum = str(data['main']['temp_max'])
+    description = data['weather'][0]['description'].capitalize()
+    wind_speed = str(data['wind']['speed'])
 
-        # create variables to store the json information
-        temp = str(int(data['main']['temp'])) + "°c "
-        minimum = str(data['main']['temp_min']) + "°c "
-        maximum = str(data['main']['temp_max']) + "°c "
-        description = data['weather'][0]['description'].capitalize()
+    # write the temperature data to the csv
+    file.write("," + temp + "," + minimum + "," + maximum
+               + "," + realfeel + "," + wind_speed + "," + description + "\n")
 
-        # write the temperature data to the csv
-        file.write("," + temp + "," + minimum + "," + maximum + "," + description + "\n")
 
-    def getBikeData(self):
-        """Function to requests and Parse Dubline bike json data"""
-        # set url avriable to the json link
-        url = "https://api.jcdecaux.com/vls/v1/stations?contract=dublin&apiKey=385fba26ba47521eaa2c4c81e0428103ab00bc13"
+def getBikeData():
+    """Function to requests and Parse Dublin bike json data"""
+    # set url avriable to the json link
+    url = "https://api.jcdecaux.com/vls/v1/stations?contract=dublin&apiKey=385fba26ba47521eaa2c4c81e0428103ab00bc13"
 
-        # use the requests library to request the data from the url
-        r = requests.get(url)
+    # use the requests library to request the data from the url
+    r = requests.get(url)
 
-        # use the json library to parse the url
-        data = json.loads(r.text)
+    # use the json library to parse the url
+    data = json.loads(r.text)
 
-        # create the headers for the csv
-        file.write("Number," + "Adress," + "lat," + "Long,"
-                   + "Banking," + "Bike Stands," + "Available Spaces,"
-                   + "Available bikes," + "Status,"
-                   + "Temperature," + "Max," + "Min,"
-                   + "Description\n")
+    # create the headers for the csv
+    file.write("Number," + "Bike Stands,"
+               + "Available Spaces,"
+               + "Available bikes," + "Status,"
+               + "Temperature," + "Max,"
+               + "Min," + "Real Feel,"
+               + "Wind Speed," + "Description\n")
 
-        # loop through the data to extract the relevant fields
-        for i in range(len(data)):
-            stopNumber = str(data[i]["number"])
-            address = data[i]["address"]
-            banking = str(data[i]["banking"])
-            bike_stands = str(data[i]["bike_stands"])
-            available_bs = str(data[i]["available_bike_stands"])
-            available_bikes = str(data[i]["available_bikes"])
-            status = data[i]["status"]
+    # loop through the data to extract the relevant fields
+    for i in range(len(data)):
+        stopNumber = str(data[i]["number"])
+        bike_stands = str(data[i]["bike_stands"])
+        available_bs = str(data[i]["available_bike_stands"])
+        available_bikes = str(data[i]["available_bikes"])
+        status = data[i]["status"]
 
-            # get the lat and long coordinates
-            position = data[i]["position"]
-            lat = str(position["lat"])
-            lng = str(position["lng"])
+        # get the lat and long coordinates
+        position = data[i]["position"]
+        lat = str(position["lat"])
+        lng = str(position["lng"])
 
-            # Write the data to the csv file
-            file.write(stopNumber + "," + address + "," + lat + "," + lng + ","
-                       + banking + "," + bike_stands + "," + available_bs + ","
-                       + available_bikes + "," + status)
+        # Write the data to the csv file
+        file.write(stopNumber + "," + bike_stands + "," + available_bs + ","
+                   + available_bikes + "," + status)
 
-            # get weather for each stop using coordinates
-            self.currentWeather(lat, lng)
+        # call currentWeather function to get weather for each stop using coordinates
+        currentWeather(lat, lng)
 
-        # close the file
-
-        print("Finished")
-
+    # close the file
+    file.close()
+    print("Finished")
 
 if __name__ == '__main__':
-    bike = BikeData()
-
-
-
+    getBikeData()
