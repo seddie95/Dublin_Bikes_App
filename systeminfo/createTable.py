a3f3f5df0dba0db43c200418b17db7c7d3b75862
seddie95 <59086410+seddie95@users.noreply.github.com>
2020-02-13 16:51:19 +0000

    updated headers

--- systeminfo/createTable.py
+++ systeminfo/createTable.py
@@ -10,7 +10,9 @@ db_name = "comp30830"
 
 # https://www.youtube.com/watch?v=kITMu17WoH4&list=PLtm3q4A2l--IEPcHhsoDpC5Yh1aLaRc0D&index=2
 #
-# headers for the table
-# (Number,Bike_Stands,Available_Spaces,Available_bikes,Status)
-#
+# headers for the bike data table
+# (bID,Number,Bike_Stands,Available_Spaces,Available_bikes,Status)
+
+#(wID, Temperature, Max,Min, Real_Feel,Wind,Speed, Description,Day, Date, Time)
+
 # all ints except status which is text
