cf78e411a34d9c5709cc23042b7c06935847df44
seddie95 <59086410+seddie95@users.noreply.github.com>
2020-02-23 14:45:48 +0000

    Updated the package setup.py script to run just the main.py script when 'bikeScrape' no quotes is entered as a command,  the main.py script Should be updated to include the weather scraping

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
-setup(name="systeminfo",
+setup(name="GetData",
       version="0.1",
-      description="First scrape of Bike data",
-      url="",
-      author="Stephen Edwards",
+      description="Bike and Weather Data scrape",
+      url="https://github.com/seddie95/comp30830",
+      author="Group 3: Alice Moyon, Dennis Kroner,Stephen Edwards",
       licence="GPL3",
-      install_requires=['requests'],
-      packages=['systeminfo'],
+      install_requires=['requests', 'mysql-connector-python'],
+      packages=['GetData'],
       entry_points={
-        'console_scripts':['bikeScrape=systeminfo.main:main']
-        }
+          'console_scripts': ['bikeScrape=GetData.main:main']
+      }
       )
