2dfad2515bbed4e06abd807cc78bd3459f5b92c4
seddie95 <59086410+seddie95@users.noreply.github.com>
2020-03-02 22:35:38 +0000

    Add Comments for readability

--- flaskApp/static/javascript/weather.js
+++ flaskApp/static/javascript/weather.js
@@ -1,43 +1,46 @@
- image_dict = {
+// dictionary with weather types and corresponding weather icons paths
+image_dict = {
     'Clear': "/static//icons/Sun.png",
     'Clouds': "/static//icons/Cloud.png",
     'Rain': "/static//icons/Rain.png",
     'Snow': "/static//icons/Snow.png",
     'Thunderstorm': "/static//icons/Storm.png",
     'fog': "/static//icons/Haze.png",
-    'Mist':"/static//icons/Haze.png",
-	'Smoke':"/static//icons/Haze.png",
-	'Haze':"/static//icons/Haze.png",
-	'Dust':"/static//icons/Haze.png",
-	'Fog':"/static//icons/Haze.png",
-	'Sand':"/static//icons/Haze.png",
-	'Dust':"/static//icons/Haze.png",
-	'Ash':"/static//icons/Haze.png",
-	'Squall':"/static//icons/Haze.png",
+    'Mist': "/static//icons/Haze.png",
+    'Smoke': "/static//icons/Haze.png",
+    'Haze': "/static//icons/Haze.png",
+    'Dust': "/static//icons/Haze.png",
+    'Fog': "/static//icons/Haze.png",
+    'Sand': "/static//icons/Haze.png",
+    'Dust': "/static//icons/Haze.png",
+    'Ash': "/static//icons/Haze.png",
+    'Squall': "/static//icons/Haze.png",
 }
 
 
 
- function fetchWeather() {
- fetch('http://api.openweathermap.org/data/2.5/weather?id=7778677&appid=9da3d1abfb8e1a3677d26c96350597c3&units=metric')
-     .then(function(response) {
-                return response.json();
-            }).then(function(obj) {
-                weathertype = obj.weather[0].main
-                tempVal = obj.main.temp
-                temp= parseInt(tempVal).toString() +"°c"
+function fetchWeather() {
+    // function to call the weather api and display weather icons and temperature in html
+    fetch('http://api.openweathermap.org/data/2.5/weather?id=7778677&appid=9da3d1abfb8e1a3677d26c96350597c3&units=metric')
+        .then(function (response) {
+            return response.json();
+        }).then(function (obj) {
+            // select the weather data from the objects
+            weathertype = obj.weather[0].main
+            tempVal = obj.main.temp
+            temp = parseInt(tempVal).toString() + "°c"
 
-                console.log(weathertype +" "  + temp)
-
-                if (weathertype === "Rain"){
-                console.log("weathertype is "+ weathertype)
-                }
-
-                if(weathertype in image_dict ){
+            // display the weather icon that correspodns to the weather type
+            if (weathertype in image_dict) {
                 document.getElementById("weatherIcon").src = image_dict[weathertype];
-                document.getElementById("temperature").innerHTML =temp;
-                }
 
-                })
-                }
-          fetchWeather()
+            }
+            // if the weather type is not available display clouds by default
+            else {
+                document.getElementById("weatherIcon").src = image_dict.Clouds;
+            }
+            // display the temperature by updating the html of the span with id "temperature"
+            document.getElementById("temperature").innerHTML = temp;
+        })
+}
+fetchWeather()
