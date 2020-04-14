5b082a26926c265cc9d7ae6e5be00d7bf8b4d2c5
Alice Moyon <aliceamoyon@gmail.com>
2020-04-14 16:36:36 +0100

    Update scheduler for new environment

--- scheduler.sh
+++ scheduler.sh
@@ -2,8 +2,8 @@
 
 echo "started cronupdate"
 crontab -l > cronupdate
-echo "*/5 * * * * /home/ubuntu/miniconda3/envs/comp30830/bin/bikeScrape >> cronlog_bike"  >> cronupdate
-echo "*/5 * * * * /home/ubuntu/miniconda3/envs/comp30830/bin/weatherScrape >> cronlog_weather" >> cronupdate
+echo "*/5 * * * * /home/ubuntu/anaconda3/envs/comp30830py37/bin/bikeScrape >> cronlog_bike"  >> cronupdate
+echo "*/5 * * * * /home/ubuntu/anaconda3/envs/comp30830py37/bin/weatherScrape >> cronlog_weather" >> cronupdate
 crontab cronupdate
 rm cronupdate
 echo "finished cronupdate"
